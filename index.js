e268f8fa0c40a7430d52d30817aff10fadc12e61
Marcel Balint <blank_blankness@yahoo.com>
2022-09-24 07:50:05 -0400

    Added routing

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const http = require("http");
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const laptopData = JSON.parse(json);
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  console.log("Server access");
+  const pathName = url.parse(req.url, true).pathname;
+  const id = url.parse(req.url, true).query.id;
+
+  if (pathName === "/products" || pathName === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end("Products page");
+  } else if (pathName === "/laptop" && id < laptopData.length) {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(`Laptop page for laptop nr. : ${id}`);
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("Url not found");
+  }
 });
 server.listen(1337, "127.0.0.1", () => {
   console.log("Listening for requests");
